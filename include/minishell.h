eab87f5e37e76a3e9cbdccacf31c09db3981d432
etienne-hd <hode.etienne@gmail.com>
2025-11-26 21:05:49 +0100

    Added: t_ctx that contains all of the minishell struct like env dict...

--- include/minishell.h
+++ include/minishell.h
@@ -6,11 +6,17 @@
 /*   By: ehode <ehode@student.42angouleme.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/16 16:53:58 by ehode             #+#    #+#             */
-/*   Updated: 2025/11/16 16:54:45 by ehode            ###   ########.fr       */
+/*   Updated: 2025/11/26 18:57:40 by ehode            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+#include "libft.h"
+
+typedef struct s_ctx
+{
+	t_dict	*env;
+}				t_ctx;
 
 #endif
