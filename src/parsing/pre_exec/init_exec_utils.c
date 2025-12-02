dd736fb63e9ceee7d9caf5829f7c6b311a613fa5
etienne-hd <hode.etienne@gmail.com>
2025-12-03 00:29:13 +0100

    Fix: added OUT_FILE_APPEND in is_redirection

--- src/parsing/pre_exec/init_exec_utils.c
+++ src/parsing/pre_exec/init_exec_utils.c
@@ -6,16 +6,17 @@
 /*   By: ehode <ehode@student.42angouleme.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 18:42:36 by ehode             #+#    #+#             */
-/*   Updated: 2025/12/02 18:42:50 by ehode            ###   ########.fr       */
+/*   Updated: 2025/12/03 00:28:25 by ehode            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "exec.h"
+#include "token.h"
+#include "parsing.h"
 
 int	is_redirection(t_token *token)
 {
 	if (token->type == IN_FILE || token->type == IN_HERE_DOC
-			|| token->type == OUT_FILE || token->type == OUT_FILE)
+			|| token->type == OUT_FILE || token->type == OUT_FILE_APPEND)
 		return (1);
 	return (0);
 }
