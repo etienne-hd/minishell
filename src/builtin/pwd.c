a7704c8821539a7a2034b382c23c4322ee2e27da
nolanCrrd <nolan.crrd@gmail.com>
2025-12-03 15:31:47 +0100

    Fix: builtin fd out

--- src/builtin/pwd.c
+++ src/builtin/pwd.c
@@ -56,7 +56,7 @@ int	pwd(t_process *process, t_ctx ctx)
 	pwd = get_pwd();
 	if (pwd == NULL)
 		return (FAILURE);
-	ft_printf("%s\n", pwd);
+	ft_dprintf(process->file_out->fd ,"%s\n", pwd);
 	free(pwd);
 	return (SUCCESS);
 }
